5cb53ee372cd7f058c688d28c627561d2de51a64
Jonnel Benjamin <jonnel.benjamin@gmail.com>
2018-09-29 18:41:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,3 +9,10 @@ def display_board
 
 end
   
+  def display_board
+    puts " "+" | "+" "+"| "+" "
+2.3.1 :021?>     puts "  |  |  "
+2.3.1 :022?>     puts
+2.3.1 :023?>     puts
+2.3.1 :024?>     puts
+2.3.1 :025?>   end
